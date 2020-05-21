45f6c40a6814b7d5243c61287bf749d6c3ee5bf7
Kuddleman <edl1155@gmail.com>
2020-05-20 19:53:06 -0700

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,91 @@
+package com.company;
+class Movie {
+    private String name;
+
+    public Movie(String name) {
+        this.name = name;
+    }
+
+    public String plot() {
+        return "No plot here!";
+    }
+}
+
+class Jaws extends Movie {
+    public Jaws() {
+        super("Jaws");
+    }
+    public String plot() {
+        return "A shark eats lots of people.";
+    }
+}
+
+class IndependenceDay extends Movie {
+
+    public IndependenceDay() {
+        super("Independence Day");
+    }
+
+
+    @Override
+    public String plot() {
+        return "Aliens attempt to take over planet Earth.";
+    }
+}
+
+class MazeRunner extends Movie {
+    public MazeRunner() {
+        super("Maze Runner");
+    }
+
+    @Override
+    public String plot() {
+        return "Kids try and excape a maze.";
+    }
+}
+
+class StarWars extends Movie {
+    public StarWars() {
+        super("Star Wars");
+    }
+
+    @Override
+    public String plot() {
+        return "Imperial forces try to take over the universe.";
+    }
+}
+
+class Forgetable extends Movie {
+    public Forgetable(){
+        super("Forgetable");
+    }
+
+    //no plot method
+}
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+    public static Movie randomMovie() {
+        //return a random movie
+        //generate a random number between 1 and 5 (inclusive)
+        int randomNumber = (int) (Math.random() * 5) + 1;
+        System.out.println("Random number generated was: " + randomNumber);
+
+        switch (randomNumber) {
+            case 1:
+                return new Jaws();
+            case 2:
+                return new IndependenceDay();
+            case 3:
+                return new MazeRunner();
+            case 4:
+                return new StarWars();
+            case 5:
+                return new Forgetable();
+        }
+    }
+}
